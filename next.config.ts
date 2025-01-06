da2bb03ca728f50c3c309844db6c466d9511622a
satyam-kalihari <satyamkalihari17@gmail.com>
2025-01-06 20:28:45 +0530

    npm run build working

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
+      "s.gravatar.com",
       "encrypted-tbn0.gstatic.com",
       "lh3.googleusercontent.com", // Allow this domain for external images
     ],
